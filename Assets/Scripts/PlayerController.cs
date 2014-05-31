6246997ee57e7a134b88b418e431bb5869b02338
Kristofer Castro <kkc47@sfu.ca>
2014-05-31 14:47:49 -0700

    Test add code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+
+	public AudioClip running;
+
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("This is a test message");
 	}
 	
 	// Update is called once per frame
