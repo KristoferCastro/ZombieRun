2f7b12be033760f4b99f8e0b3af8c0e2353ab2c3
Kristofer Castro <kkc47@sfu.ca>
2014-06-10 23:28:39 -0700

    Destroy powerups timer
    powerups disappear after 5 seconds

--- Assets/Scripts/SpeedPowerup.cs
+++ Assets/Scripts/SpeedPowerup.cs
@@ -5,7 +5,11 @@ public class SpeedPowerup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		DeleteAfter (5);
+	}
 	
+	void DeleteAfter(float seconds){
+		Destroy(this.gameObject, seconds);
 	}
 	
 	// Update is called once per frame
