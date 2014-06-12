3ff6c6f6049ad13d413480339639baaed5c0e66a
Jason <jyc8@live.com>
2014-06-11 19:58:31 -0700

    GO Scene - Screen Fade Out

--- Assets/Scripts/Jason/LoadSceneOnKey.cs
+++ Assets/Scripts/Jason/LoadSceneOnKey.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class LoadSceneOnKey : MonoBehaviour {
 
+	public GameObject blankImage;
 	public string level = "Scene Name"; 
 	bool fading = false;
 
 	void Update () {
 		if(Input.GetKeyDown("space") && !fading){
 			fading = true;
+			blankImage.SetActive(true);
 			StartCoroutine(FadeMusicLoadLevel()); 
 		}
 	}
@@ -16,10 +18,9 @@ public class LoadSceneOnKey : MonoBehaviour {
 	IEnumerator FadeMusicLoadLevel(){
 		for (float i = 9; i > 0; i--){
 			audio.volume = audio.volume - audio.volume * 0.3f;
-			yield return new WaitForSeconds (0.5f);		
+			yield return new WaitForSeconds (0.5f);
 		}
 		Application.LoadLevel(level);
-
 	}
 
 }
