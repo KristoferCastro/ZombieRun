d6c7ea06810d3b47564b06fca10ad0562cf957ef
Jason <jyc8@live.com>
2014-06-11 21:18:22 -0700

    Many Changes yo

---
+++ Assets/Scripts/Jason/OnCollisionWin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollisionWin : MonoBehaviour {
+	
+	public GameObject player;
+	public GameObject fade;
+	public GameObject spawners;
+	public string tag = "Player";
+	public string level = "Scene Name";
+	bool winning = false;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == tag && !winning){
+			StartCoroutine(Win()); 
+		}
+	}
+	
+	IEnumerator Win(){
+		winning = true;
+		//fade.SetActive(true);
+		//player.audio.Play();
+		spawners.SetActive(false);
+
+		player.GetComponent<PlayerController>().enabled = false;
+		player.animation.Play();
+		GameObject.Find("ProgressHUD").SetActive(false);
+
+		yield return new WaitForSeconds (5f);
+		Application.LoadLevel(level);
+	}
+}
