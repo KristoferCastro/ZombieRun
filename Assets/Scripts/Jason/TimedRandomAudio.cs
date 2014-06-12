cf632ed0980d49abe7e3641be7f0e58969198b63
Jason <jyc8@live.com>
2014-06-11 18:21:19 -0700

    Moaaaan

---
+++ Assets/Scripts/Jason/TimedRandomAudio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedRandomAudio : MonoBehaviour {
+
+	public AudioClip[] sound;
+	public float seconds = 3;
+	
+	void Start(){
+		InvokeRepeating("PlayClipAndChange",0.2f * Random.Range(0, 20),seconds);
+	}
+	
+	void PlayClipAndChange(){
+		audio.clip = sound[Random.Range(0, sound.Length)];
+		audio.Play();
+	}
+}
