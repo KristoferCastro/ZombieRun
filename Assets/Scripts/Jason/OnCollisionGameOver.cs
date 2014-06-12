d4ef530526f1ec7a7e90ac1a11edfcb410677d4e
Jason <jyc8@live.com>
2014-06-11 20:50:43 -0700

    Changes

--- Assets/Scripts/Jason/OnCollisionGameOver.cs
+++ Assets/Scripts/Jason/OnCollisionGameOver.cs
@@ -6,20 +6,22 @@ public class OnCollisionGameOver : MonoBehaviour {
 	public GameObject player;
 	public GameObject fade;
 	public string tag = "Player";
-	public string level = "Scene Name"; 
+	public string level = "Scene Name";
+	bool fading = false;
 	
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.gameObject.tag == tag){
+		if (other.gameObject.tag == tag && !fade){
 			StartCoroutine(Death()); 
 		}
 	}
 
 	IEnumerator Death(){
+		fading = true;
+		fade.SetActive(true);
 		player.audio.Play();
 		player.GetComponent<PlayerController>().enabled = false;
 		GameObject.Find("ProgressHUD").SetActive(false);
-		fade.SetActive(true);
 		yield return new WaitForSeconds (3f);
 		Application.LoadLevel(level);
 	}
